3239e89175e48f3dfa6a598e77b5e4aabf08700c
Esteban Madrid <53920121+misaa023@users.noreply.github.com>
2024-09-10 14:29:00 -0300

    Update script.js
    elimine console log de data

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 const getProductos = async () => {
     const response = await fetch('data.json');
     const data = await response.json();
-    console.log(data);
+    
 
     data.forEach((producto) => {
         let contenido = document.createElement('div');
